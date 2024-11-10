9baaff72210399f042df26847370b4f7bbbb0e32
Facu Bustos <Facubustos2107@gmail.com>
2024-11-09 22:01:31 -0300

    Update Usuario.java

--- src/Clases/Usuario.java
+++ src/Clases/Usuario.java
@@ -1,6 +1,9 @@
 package Clases;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+import java.util.Objects;
 
 public abstract class Usuario extends Persona{
 
@@ -8,52 +11,60 @@ public abstract class Usuario extends Persona{
 
     private String email;
     private String contrasenia;
-
+    private List<Vuelo> listaVuelos;
     //CONSTRUCTOR
 
 
-    public Usuario(String telefono, String fechaNacimiento, String nacionalidad, String apellido, String DNI, String direccion, String nombre, String email, String contrasenia) {
-        super(telefono, fechaNacimiento, nacionalidad, apellido, DNI, direccion, nombre);
+    public Usuario(String nombre, String apellido, String DNI, String direccion, String nacionalidad, String telefono, String fechaNacimiento, String email, String contrasenia, List<Vuelo> listaVuelos) {
+        super(nombre, apellido, DNI, direccion, nacionalidad, telefono, fechaNacimiento);
         this.email = email;
         this.contrasenia = contrasenia;
+        this.listaVuelos = new ArrayList<>();
     }
-
-    public Usuario(String email, String contrasenia) {
-        this.email = email;
-        this.contrasenia = contrasenia;
-    }
-
-    public Usuario(){
-
-    }
-
-    //GETTER
 
     public String getEmail() {
         return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public String getContrasenia() {
         return contrasenia;
     }
 
-    //SETTER
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
     public void setContrasenia(String contrasenia) {
         this.contrasenia = contrasenia;
     }
 
-    //METODOS
+    public List<Vuelo> getListaVuelos() {
+        return listaVuelos;
+    }
+
+    public void setListaVuelos(List<Vuelo> listaVuelos) {
+        this.listaVuelos = listaVuelos;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Usuario usuario)) return false;
+        if (!super.equals(o)) return false;
+        return Objects.equals(email, usuario.email) && Objects.equals(contrasenia, usuario.contrasenia) && Objects.equals(listaVuelos, usuario.listaVuelos);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), email, contrasenia, listaVuelos);
+    }
 
     @Override
     public String toString() {
         return "Usuario{" +
                 "email='" + email + '\'' +
-                ", contrasenia='" + contrasenia  + super.toString() +
-                '}';
+                ", contrasenia='" + contrasenia + '\'' +
+                ", listaVuelos=" + listaVuelos +
+                "} " + super.toString();
     }
 }
