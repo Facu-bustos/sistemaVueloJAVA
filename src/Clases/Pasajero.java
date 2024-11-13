757ffeb815d3fef66061072faed9b2e95fdc1e85
Facu Bustos <Facubustos2107@gmail.com>
2024-11-12 21:55:50 -0300

    mmm
    Commit no terminado

--- src/Clases/Pasajero.java
+++ src/Clases/Pasajero.java
@@ -3,6 +3,8 @@ import Enumeradores.TipoEstadoCheckIn;
 import Enumeradores.TipoPasajero;
 import Excepciones.ReadFail;
 import org.json.JSONException;
+
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.Scanner;
@@ -17,22 +19,25 @@ public class Pasajero extends Usuario {
     private String numPasaporte;
     private TipoPasajero tipoPasajero;
     private TipoEstadoCheckIn estadoCheckIn;
+    private Integer idVueloPasajero;
     Scanner scanner;
 
     //CONSTRUCTOR
-
-
-    public Pasajero(String nombre, String apellido, String DNI, String direccion, String nacionalidad, String telefono, String fechaNacimiento, String email, String contrasenia, List<Vuelo> listaVuelos, String numPasaporte, TipoPasajero tipoPasajero, TipoEstadoCheckIn estadoCheckIn, Scanner scanner) {
+    public Pasajero(String nombre, String apellido, String DNI, String direccion, String nacionalidad, String telefono, String fechaNacimiento, String email, String contrasenia, List<Vuelo> listaVuelos, String numPasaporte, TipoPasajero tipoPasajero, TipoEstadoCheckIn estadoCheckIn, Integer idVueloPasajero, Scanner scanner) {
         super(nombre, apellido, DNI, direccion, nacionalidad, telefono, fechaNacimiento, email, contrasenia, listaVuelos);
         this.numPasaporte = numPasaporte;
         this.tipoPasajero = tipoPasajero;
         this.estadoCheckIn = estadoCheckIn;
-        this.scanner = new Scanner(System.in);
+        this.idVueloPasajero = idVueloPasajero;
+        this.scanner = scanner;
     }
 
-    public Pasajero() {
-        super();
+    public Pasajero()
+    {
+
     }
+    //Gette and setter
+
 
     public String getNumPasaporte() {
         return numPasaporte;
@@ -58,6 +63,14 @@ public class Pasajero extends Usuario {
         this.estadoCheckIn = estadoCheckIn;
     }
 
+    public Integer getIdVueloPasajero() {
+        return idVueloPasajero;
+    }
+
+    public void setIdVueloPasajero(Integer idVueloPasajero) {
+        this.idVueloPasajero = idVueloPasajero;
+    }
+
     public Scanner getScanner() {
         return scanner;
     }
@@ -71,14 +84,15 @@ public class Pasajero extends Usuario {
         if (this == o) return true;
         if (!(o instanceof Pasajero pasajero)) return false;
         if (!super.equals(o)) return false;
-        return Objects.equals(numPasaporte, pasajero.numPasaporte) && tipoPasajero == pasajero.tipoPasajero && estadoCheckIn == pasajero.estadoCheckIn && Objects.equals(scanner, pasajero.scanner);
+        return Objects.equals(numPasaporte, pasajero.numPasaporte) && tipoPasajero == pasajero.tipoPasajero && estadoCheckIn == pasajero.estadoCheckIn && Objects.equals(idVueloPasajero, pasajero.idVueloPasajero) && Objects.equals(scanner, pasajero.scanner);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(super.hashCode(), numPasaporte, tipoPasajero, estadoCheckIn, scanner);
+        return Objects.hash(super.hashCode(), numPasaporte, tipoPasajero, estadoCheckIn, idVueloPasajero, scanner);
     }
 
+    ///METODOS
     public List<Vuelo> lecturaDeListaVuelo()
     {
         try {
@@ -91,23 +105,24 @@ public class Pasajero extends Usuario {
         }
     }
 
-
     @Override
     public String toString() {
         return "Pasajero{" +
                 "numPasaporte='" + numPasaporte + '\'' +
                 ", tipoPasajero=" + tipoPasajero +
                 ", estadoCheckIn=" + estadoCheckIn +
+                ", idVueloPasajero=" + idVueloPasajero +
                 ", scanner=" + scanner +
                 "} " + super.toString();
     }
 
-    public static Integer compraDeVuelo(List<Vuelo>listaVuelo) throws JSONException {
+    public static List<Vuelo> compraDeVuelo() throws JSONException {
 
         Scanner scanner1 = new Scanner(System.in);
+        List<Vuelo>listaVuelosDisponibles=GestionJSON.mapeoVuelo();
+        List<Vuelo>ListaDeTickets=new ArrayList<>();
         System.out.println("Elija el ID del vuelo a comprar:");
         String id = scanner1.nextLine();
-        Integer idBusqueda=null;
         // Verificamos si la entrada es un número
         if (!id.matches("\\d+")) {  // Verificamos que contenga solo dígitos
             System.out.println("Error: Debe ingresar un número válido.");
@@ -115,14 +130,38 @@ public class Pasajero extends Usuario {
             int idVuelo = Integer.parseInt(id);  // Convertir a número
             boolean encontrado = false;
             // buscamos el vuelo en la lista de vuelos , si hay coincidencia accedemos a persistir
-            for (Vuelo v : listaVuelo) {
+            for (Vuelo v : listaVuelosDisponibles) {
                 if (v.getIdVuelo() == idVuelo) {
                     System.out.println("Vuelo encontrado:");
-                    createJSON(idVuelo,listaVuelo);
+                    for(int i=0; i<listaVuelosDisponibles.size(); i++)
+                    {
+                        Vuelo ticketVuelo=new Vuelo();
+                        ticketVuelo.setIdVuelo(v.getIdVuelo());
+                        ticketVuelo.setOrigen(v.getOrigen());
+                        ticketVuelo.setDestino(v.getDestino());
+                        ticketVuelo.setHoraSalida(v.getHoraSalida());
+                        ticketVuelo.setHoraLlegada(v.getHoraLlegada());
+                        ticketVuelo.setDuracion(v.getDuracion());
+                        ticketVuelo.setPrecio(v.getPrecio());
+                        ticketVuelo.setCantidadDisponible(v.getCantidadDisponible());
+                        ticketVuelo.setAerolinea(v.getAerolinea());
+                        ticketVuelo.setClase(v.getClase());
+                        ticketVuelo.setNumeroVuelo(v.getNumeroVuelo());
+                        List<Escala>escalaVuelo=new ArrayList<>();
+                        for(Escala e: v.getEscalas())
+                        {
+                            Escala escalaV= new Escala();
+                            escalaV.setAeropueto(e.getAeropueto());
+                            escalaV.setHoraSalida(e.getHoraSalida());
+                            escalaVuelo.add(escalaV);
+                        }
+                        ticketVuelo.setEscalas(escalaVuelo);
+                        ticketVuelo.setTipoVuelo(v.getTipoVuelo());
+                        ticketVuelo.setEstadoVuelo(v.getEstadoVuelo());
+                        ListaDeTickets.add(ticketVuelo);
+                    }
                     System.out.println("Vuelo comprado:");
-                    datosDelViajeAdquirido(idVuelo,listaVuelo);
                     encontrado = true;
-                    idBusqueda=idVuelo;
                     break;
                 }
 
@@ -131,12 +170,17 @@ public class Pasajero extends Usuario {
                 System.out.println("Error: No existe un vuelo con el ID " + idVuelo + ".");
             }
         }
-        return idBusqueda;
+        return ListaDeTickets ;
     }
 
-    public void cancelacionDeVuelo()
-    {
-
+    public void cancelacionDeVuelo(Integer id) throws JSONException {
+        System.out.println("¿Desea eliminar el vuelo?");
+        String si=scanner.nextLine();
+        if(si.equalsIgnoreCase("si"))
+        {
+            ReservasPasajeros p = new ReservasPasajeros();
+            p.cancelacionDeVuelo(id);
+        }
     }
 
     public void mostrarDatosPersonales()
